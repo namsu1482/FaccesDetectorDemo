ee91436adc3e5172589a5945f6973acabe8d5da4
namsu choi <namsu1482@gmail.com>
2021-01-22 16:13:48 +0900

    init

---
+++ app/src/main/java/com/example/faccesdetectordemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.faccesdetectordemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.FaceDetector;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
